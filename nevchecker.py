86625835b0523c7fda0143e94bd89f21ff978478
Devllench <lenya2012@mail.ru>
2020-03-04 19:11:01 +0300

    чтения файлов из файла, определение расширения файла

--- nevchecker.py
+++ nevchecker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+import csv
+import os
 
 print("hello")
 
@@ -27,11 +29,25 @@ else:
 
 
 # собрать массив из файла кастомера
-def create_client_list(client_file):
+def read_client_file_txt(client_file):
     with open(client_file) as file:
         client_list = [row.strip() for row in file]
     return client_list
 
+def read_client_file_csv(client_file):
+    pass
+
+def create_client_list(client_file):
+    ext = os.path.splitext(client_file)
+    print(str(ext))
+    if ".txt" in ext:
+        print("file in "+str(ext[1]))
+        client_list = read_client_file_txt(client_file)
+    elif ".csv" in ext:
+        print("file in "+str(ext[1]))
+        client_list = read_client_file_csv(client_file)
+
+    return client_list
 
 # собрать массив из нашешл (из логов)
 def create_beget_list_from_file(beget_file):
@@ -46,18 +62,21 @@ def create_beget_list_from_file(beget_file):
 def comparison_lists(client_list, beget_list):
     delete_email = []
     dont_delete_email = []
+    check1 = 0
+    check2 = 0
+
     for email_beget in beget_list:
         if email_beget in client_list:
             delete_email.append(email_beget)
         elif email_beget not in client_list:
             dont_delete_email.append(email_beget)
-    check1 = 0
-    check2 = 0
+
     for i in delete_email:
         check1 = check1 + 1
     for j in dont_delete_email:
         check2 = check2 + 1
 
+# вывести результат( сколько ящиков из списка кастомера совпало с нашим списком)
     print("удалены "+str(delete_email))
     print("Всего удалено - " + str(check1))
     print("не удалены "+str(dont_delete_email))
@@ -65,10 +84,14 @@ def comparison_lists(client_list, beget_list):
     input("Press Enter to Continue. . .: ")
 
 
-# вывести результат( сколько ящиков из списка кастомера совпало с нашим списком)
+
 # main
 
 blist1 = create_beget_list_from_file(str(args.beget_file))
 clist2 = create_client_list(str(args.customer_file))
 
+print(":-"+str(clist2))
+input("Press Enter to Continue. . .: ")
+
 comparison_lists(clist2, blist1)
+
